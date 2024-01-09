2a5035526a2c6a9b3e90339dbc7b0ab977e595ac
Lamees Anees <lamees.anees@gmail.com>
2024-01-09 10:23:51 +0530

    second

--- age.js
+++
@@ -1,5 +0,0 @@
-var name = prompt('Enter Your Name:');
-var age = prompt('Enter Your Age:');
-agedays = age * 365.25
-remainingdays = (60 - age) * 365.25;
-alert("Hello " + name + "...\nYour Current Age:" + agedays + " days\nYou will be there on the earth only " + remainingdays + " days."); 
